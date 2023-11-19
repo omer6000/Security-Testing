5b0887e17a5c8515bcc8e570e0b0554885710a57
omer6000 <omeriqbal7@hotmail.com>
2023-11-19 14:42:00 +0100

    tinyc

--- exercise_2/tinyc_grammar.py
+++ exercise_2/tinyc_grammar.py
@@ -2,31 +2,35 @@ TINYCGRAMMAR = {
     "<start>":  ["<statement>"],
     "<statement>" : ["if <paren_expr> <statement>",
                      "if <paren_expr> <statement> else <statement>",
-                     "while <paren_expr> <statement>","do <statement> while <paren_expr> <semicolon>",
-                     "<curlybracket_left> <statement><asterisk> <curlybracket_right>",
+                     "while <paren_expr> <statement>",
+                     "do <statement> while <paren_expr> <semicolon>",
+                     "<curlybracket_left> <curlybracket_left> <statement> <curlybracket_right> <curlybracket_right>",
                      "<expr> <semicolon>",
                      "<semicolon>"],
-    "<paren_expr>" : ["( <expr> )"],
+    "<paren_expr>" : ["<bracket_left> <expr> <bracket_right>"],
     "<expr>" : ["<test>","<id> <equal> <expr>"],
     "<test>" : ["<sum>","<sum> <less> <sum>"],
-    "<sum>" : ["<term>","<sum> + <term>", "<sum> - <term>"],
+    "<sum>" : ["<term>","<sum> <plus> <term>", "<sum> <minus> <term>"],
     "<term>": ["<id>","<int>","<paren_expr>"],
-    "<id>": ["/[a-z]+/"],
-    "<int>": ["/[-+]?(\d*\.\d+|\d+\.\d*)([eE][-+]?\d+)?/"],
+    "<id>": [chr(i) for i in range(97, 123)],
+    "<int>": ["<int><number>","<number>"],
+    "<number>": [str(i) for i in range(10)],
     "<semicolon>" : [";"],
-    "<asterisk>": ["*"],
-    "<curlybracket_left>": "{",
-    "<curlybracket_right>": "}",
-    "<less>" : "<",
-    "<equal>": "="
-
-    
+    "<curlybracket_left>": ["{"],
+    "<curlybracket_right>": ["}"],
+    "<less>" : ["<"],
+    "<equal>": ["="],
+    "<plus>": ["+"],
+    "<minus>": ["-"],
+    "<bracket_left>": ["("],
+    "<bracket_right>": [")"]
 }
 
 from fuzzingbook.Grammars import simple_grammar_fuzzer
-
-a = simple_grammar_fuzzer(TINYCGRAMMAR)
-print(a)
+import random
+# random.seed()
+# a = simple_grammar_fuzzer(TINYCGRAMMAR)
+# print(a)
 
 #                 "{" <statement>* "}" |
 #                 <expr> ";" |
