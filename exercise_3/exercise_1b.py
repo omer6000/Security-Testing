9b0495e8861dfbe42e20f04a0ed8fc27e38e175e
omer6000 <omeriqbal7@hotmail.com>
2023-11-25 20:05:05 +0100

    exercise 3 part 1b

--- exercise_3/exercise_1b.py
+++ exercise_3/exercise_1b.py
@@ -1,6 +1,17 @@
 """
 Use this file to implement your solution for exercise 3-1 b.
 """
+import random
+from exercise_1a import find_subtrees
+
+def modified_tree(tree,old_subtree,new_subtree):
+    if tree == old_subtree:
+        tree = new_subtree
+    elif tree[1] is not None:
+        tree = (tree[0],[modified_tree(child,old_subtree,new_subtree) for child in tree[1]])
+    return tree
 
 def replace_random_subtree(tree, symbol, subtrees):
-    return tree
+    to_replace = random.choice(find_subtrees(tree,symbol))
+    replace_with = random.choice(subtrees)
+    return modified_tree(tree,to_replace,replace_with)
